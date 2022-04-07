547f77b648cec9f242f9eb932f5fc1d74e87e45f
kaustubhpande73 <82426976+kaustubhpande73@users.noreply.github.com>
2022-04-07 18:42:28 -0500

    Update test_email.py

--- Tests/test_email.py
+++ Tests/test_email.py
@@ -1,4 +1,19 @@
+import project1
+import pytest
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+import spacy
+from spacy import displacy
+import pandas as pd
+from nltk.corpus import wordnet
+import sys
+nlp=spacy.blank("en")
+nlp =spacy.load("en_core_web_sm")
 
+data = "release Mindhunter david.fincher@netflix.com"
 
 
 
