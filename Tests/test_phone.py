40e0919258e0c0ad19342def387e567d204afd14
kaustubhpande73 <82426976+kaustubhpande73@users.noreply.github.com>
2022-04-07 18:39:13 -0500

    Update test_phone.py

--- Tests/test_phone.py
+++ Tests/test_phone.py
@@ -16,12 +16,13 @@ nlp =spacy.load("en_core_web_sm")
 data = "my phone is 233-245-2211"
 
 def phone(data):
-text = data
-doc=nlp(text)
-text = re.sub('\n',' ',text)
-text = re.sub('\t',' ',text)
-regex_phone=r'\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]?\d{4}|\d{3}[-\.\s]??\d{4}' # format - ###-###-####
-phone=re.findall(regex_phone, text)
-for j in phone:
-    text=text.replace(j,'\u2588'*len(j))
-return text
+    text = data
+    doc=nlp(text)
+    text = re.sub('\n',' ',text)
+    text = re.sub('\t',' ',text)
+    regex_phone=r'\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]?\d{4}|\d{3}[-\.\s]??\d{4}' # format - ###-###-####
+    phone=re.findall(regex_phone, text)
+    for j in phone:
+        text=text.replace(j,'\u2588'*len(j))
+    return text
+    assert text != None
