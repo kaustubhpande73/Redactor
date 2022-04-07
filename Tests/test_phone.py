347c24a0521b1e9a5f3a312d257a1304d190b1cd
kaustubhpande73 <82426976+kaustubhpande73@users.noreply.github.com>
2022-04-07 18:38:28 -0500

    Create test_phone.py

---
+++ Tests/test_phone.py
@@ -0,0 +1,27 @@
+import project1
+import pytest
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+import spacy
+from spacy import displacy
+import pandas as pd
+from nltk.corpus import wordnet
+import sys
+nlp=spacy.blank("en")
+nlp =spacy.load("en_core_web_sm")
+
+data = "my phone is 233-245-2211"
+
+def phone(data):
+text = data
+doc=nlp(text)
+text = re.sub('\n',' ',text)
+text = re.sub('\t',' ',text)
+regex_phone=r'\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]?\d{4}|\d{3}[-\.\s]??\d{4}' # format - ###-###-####
+phone=re.findall(regex_phone, text)
+for j in phone:
+    text=text.replace(j,'\u2588'*len(j))
+return text
