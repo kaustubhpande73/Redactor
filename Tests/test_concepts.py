c5aa7fbf96413f45adec618c755f40aec3571f5d
kaustubhpande73 <82426976+kaustubhpande73@users.noreply.github.com>
2022-04-07 19:50:16 -0500

    Update test_concepts.py

--- Tests/test_concepts.py
+++ Tests/test_concepts.py
@@ -1,8 +1,17 @@
-
-
-
-
-
+import project1
+import pytest
+import nltk
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+import spacy
+from spacy import displacy
+import pandas as pd
+from nltk.corpus import wordnet
+import sys
+nlp=spacy.blank("en")
+nlp =spacy.load("en_core_web_sm")
 
 
 
